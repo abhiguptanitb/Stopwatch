b5157b0cde76c7d3b0b9aaf5663d59001cc03d3d
ABHI GUPTA <138576028+abhiguptanitb@users.noreply.github.com>
2024-06-23 17:26:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,31 +7,31 @@ const stopwatchDuration=document.querySelector("#stopwatchDuration"),
 
 let hrs=0,mins=0,sec=0,ms=0,timeInterval;
 
-start.onclick=()=>{
-    timeInterval=setInterval(()=>{
+start.addEventListener('click', () => {
+    timeInterval = setInterval(() => {
         ms++;
-        if(ms==100) {
+        if (ms == 100) {
             sec++;
-            ms=0;
+            ms = 0;
         }
-        if(sec==60) {
+        if (sec == 60) {
             mins++;
-            sec=0;
+            sec = 0;
         }
-        if(mins==60){
+        if (mins == 60) {
             hrs++;
-            mins=0;
+            mins = 0;
         }
 
-        stopwatchDuration.innerHTML=`${zeroPad(hrs)}:${zeroPad(mins)}:${zeroPad(sec)}:${zeroPad(ms)}`;
-    },10);
+        stopwatchDuration.innerHTML = `${zeroPad(hrs)}:${zeroPad(mins)}:${zeroPad(sec)}:${zeroPad(ms)}`;
+    }, 10);
 
     start.setAttribute("style","display:none");
     stop.setAttribute("style","display:block");
     lap.setAttribute("style","display:block");
     reset.setAttribute("style","display:none");
     lap.removeAttribute("disabled");
-};
+});
 
 const zeroPad=(num)=>{
     return String(num).padStart(2,"0");
@@ -39,33 +39,33 @@ const zeroPad=(num)=>{
 
 let count=0;
 
-lap.onclick=()=>{
+lap.addEventListener('click', () => {
     count++;
-    let li=document.createElement("li");
-    li.innerHTML=`${"#"+count} - ${zeroPad(hrs)}:${zeroPad(mins)}:${zeroPad(sec)}:${zeroPad(ms)}`;
+    let li = document.createElement("li");
+    li.innerHTML = `${"#" + count} - ${zeroPad(hrs)}:${zeroPad(mins)}:${zeroPad(sec)}:${zeroPad(ms)}`;
     laps.appendChild(li);
-    laps.scroll({top:laps.scrollHeight, behavior:"smooth"});
-};
+    laps.scroll({ top: laps.scrollHeight, behavior: "smooth" });
+});
 
-stop.onclick=()=>{
+stop.addEventListener('click', () => {
     clearInterval(timeInterval);
-    lap.setAttribute("style","display:none");
-    reset.setAttribute("style","display:block");
-    start.setAttribute("style","display:block");
-    start.innerHTML="Resume";
-    stop.setAttribute("style","display:none");
-};
+    lap.setAttribute("style", "display:none");
+    reset.setAttribute("style", "display:block");
+    start.setAttribute("style", "display:block");
+    start.innerHTML = "Resume";
+    stop.setAttribute("style", "display:none");
+});
 
-reset.onclick=()=>{
-    laps.innerHTML="";
-    hrs=mins=sec=ms=count=0;
+reset.addEventListener('click', () => {
+    laps.innerHTML = "";
+    hrs = mins = sec = ms = count = 0;
     clearInterval(timeInterval);
-    stopwatchDuration.innerHTML="00:00:00:00";
+    stopwatchDuration.innerHTML = "00:00:00:00";
 
-    start.innerHTML="Start";
-    lap.setAttribute("style","display:block");
-    lap.setAttribute("disabled",true);
-    reset.setAttribute("style","display:none");
-    start.setAttribute("style","display:block");
-    stop.setAttribute("style","display:none");
-};
+    start.innerHTML = "Start";
+    lap.setAttribute("style", "display:block");
+    lap.setAttribute("disabled", true);
+    reset.setAttribute("style", "display:none");
+    start.setAttribute("style", "display:block");
+    stop.setAttribute("style", "display:none");
+});
